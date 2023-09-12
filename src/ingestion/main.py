196c71005ca6e9751a57969ee540ba6e791097aa
Jack White <jfw793@gmail.com>
2023-09-12 16:38:22 +1000

    remove unnecessary class

--- src/ingestion/main.py
+++ src/ingestion/main.py
@@ -3,43 +3,44 @@ import json
 import boto3
 import requests
 from dotenv import load_dotenv
-from datetime import datetime
 
 
-class Ingestion:
-    def __init__(self, access_key, secret_key):
-        self.client = boto3.client(
-            "s3",
-            aws_access_key_id=access_key,
-            aws_secret_access_key=secret_key,
-        )
+def get_s3_client(access_key, secret_key):
+    client = boto3.client(
+        "s3",
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key,
+    )
+    return client
 
-    def get_match_data(self, affinity, puuid):
-        MATCH_DATA_ENDPOINT = f"https://api.henrikdev.xyz/valorant/v3/by-puuid/matches/{affinity}/{puuid}?mode=competitive&size=100"
-        response = requests.get(MATCH_DATA_ENDPOINT)
-        # ignore response headers, return data only
-        json_data = json.loads(response.text)["data"]
-        # encode as UTF-8 bytes for S3 upload
-        data_dump = bytes(json.dumps(json_data).encode("UTF-8"))
-        return data_dump
 
-    def upload_to_s3(self, bucket, key, file):
-        self.client.put_object(Body=file, Bucket=bucket, Key=key)
+def get_match_data(affinity, puuid):
+    MATCH_DATA_ENDPOINT = f"https://api.henrikdev.xyz/valorant/v3/by-puuid/matches/{affinity}/{puuid}?mode=competitive&size=100"
+    response = requests.get(MATCH_DATA_ENDPOINT)
+    # ignore response headers, return data only
+    json_data = json.loads(response.text)["data"]
+    # encode as UTF-8 bytes for S3 upload
+    data_dump = bytes(json.dumps(json_data).encode("UTF-8"))
+    return data_dump
+
+
+def upload_to_s3(s3_client, bucket, key, file):
+    s3_client.put_object(Body=file, Bucket=bucket, Key=key)
 
 
 def main():
     load_dotenv()
     AWS_ACCESS_KEY_ID = os.getenv("ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.getenv("SECRET_ACCESS_KEY")
-    ingestion = Ingestion(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+    s3_client = get_s3_client(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
 
     region = os.getenv("REGION")
     puuid = os.getenv("PUUID")
-    data = ingestion.get_match_data(region, puuid)
+    data = get_match_data(region, puuid)
 
     bucket_name = "valorant-data-raw"
     file_name = f"competitive-match-data.json"
-    ingestion.upload_to_s3(bucket_name, file_name, data)
+    upload_to_s3(s3_client, bucket_name, file_name, data)
 
 
 if __name__ == "__main__":
